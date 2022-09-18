557f7e4d3f3915c7b43f0799c5826b4cc1f5e854
SHINSUNGJIN <pxp13716@gmail.com>
2022-09-18 20:39:14 +0900

    .

--- Sample/Chap11_ReduxThunk/modules/index222.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import counterR from "./counterR";
-import contactR from "./contactR";
-
-const rootReducer = combineReducers({
-  counterR,
-  contactR,
-});
-export default rootReducer;
